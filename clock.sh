e1e6503629ed404285bb758d875541090120c641
Atanas Laskov <latanas@gmail.com>
2018-03-11 10:09:45 +0000

    Add atom config.

--- clock.sh
+++ clock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 10s
-xclock -d -geometry -20-0 -strftime "%Z %R, %d-%b-%y" -render -face "Liberation Mono-25:bold" &
+xclock -d -geometry -20-0 -strftime "%Z %R, %d-%b-%y" -render -face "Iosevka Slab-25:bold" &
